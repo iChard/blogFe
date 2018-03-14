3f77b1e1e238e6a81a758892dbe79892f325c4e3
liupeiyu <liupeiyu@raycloud.com>
2018-03-14 21:19:29 +0800

    delete dev code

--- node_modules/tui-chart/src/js/themes/defaultThemesRegistration.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var chartConst = require('../const');
-var themeManager = require('./themeManager');
-var defaultTheme = require('./defaultTheme');
-
-themeManager.register(chartConst.DEFAULT_THEME_NAME, defaultTheme);
